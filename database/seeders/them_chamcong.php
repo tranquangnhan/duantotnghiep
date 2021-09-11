e49850cde53420dc8cdaf4a8c94c40652f1ec3c6
Ngô Anh Quốc <quocna@sangiare.vn>
2021-09-11 20:58:04 +0700

    sửa migration

--- database/seeders/them_chamcong.php
+++ database/seeders/them_chamcong.php
@@ -17,9 +17,9 @@ class them_chamcong extends Seeder
         $checkIn = date(time());
         $checkOut = date(time()) + 2000;
         DB::table('chamcong')->insert([
-            ['checkin' => 1629421842, 'checkout' => 1629421842, 'ngay' => '2021-08-20','idns' => 6, 'trangthai' => 1],
-            ['checkin' => 1629421842 + 1000, 'checkout' => 1629421842 + 1000, 'ngay' => '2021-08-20', 'idns' => 7, 'trangthai' => 1],
-            ['checkin' => 1629421842 + 2000, 'checkout' => 1629421842 + 2000, 'ngay' => '2021-08-20', 'idns' => 8, 'trangthai' => 1]
+            ['checkin' => 1629421842, 'checkout' => 1629421842, 'ngay' => '2021-08-20','idns' => 17, 'trangthai' => 1],
+            ['checkin' => 1629421842 + 1000, 'checkout' => 1629421842 + 1000, 'ngay' => '2021-08-20', 'idns' => 18, 'trangthai' => 1],
+            ['checkin' => 1629421842 + 2000, 'checkout' => 1629421842 + 2000, 'ngay' => '2021-08-20', 'idns' => 19, 'trangthai' => 1]
         ]);
     }
 }
